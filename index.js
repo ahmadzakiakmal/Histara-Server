c05de0ccb144f399b7cf86adb378fa7b313657da
Ahmad Zaki Akmal <87590846+ahmadzakiakmal@users.noreply.github.com>
2024-05-05 16:38:59 +0700

    feat: setup database connection

--- index.js
+++ index.js
@@ -1,12 +1,29 @@
 const express = require("express");
 const morgan = require("morgan");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const process = require("process");
+
+
+dotenv.config();
 
 const app = express();
 
 app.use(morgan("dev"));
 
+if (!process.env.MONGO_URI) {
+  throw Error("Database connection string not found");
+}
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log("Succesfully connected to MongoDB");
+  }).catch((err) => {
+    console.log("Failed to connect to MongoDB");
+    console.log(err);
+  });
+
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.send("Histara Server");
 });
 
 app.listen(5000, () => {
